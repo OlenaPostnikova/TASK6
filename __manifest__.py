b29c2b1b38c75fb11adb1128293dba9f60de195f
“Olena <a4olena@gmail.com>
2023-07-29 14:54:04 +0300

    Add

--- __manifest__.py
+++ __manifest__.py
@@ -3,15 +3,14 @@
     'name': "/home/admin16/opt/odoo-16.0/repositories/olenapostnikova/task6",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        MOT and service station""",
 
     'description': """
         Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
+    'author': "Olena Postnikova",
+    'website': "https://www.a4.com.ua",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
